b4ddf0bdfe8fb628fbe9f46a488c637f98cd118d
HafiAga <ulguiorra2@gmail.com>
2022-11-21 18:40:13 +0300

    Домашнее задание 4

---
+++ Домашнее задание 4/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Write number 1: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Write number 2: ");
+        int m = int.Parse(Console.ReadLine());
+        for (int i = n; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
+    }
+}
